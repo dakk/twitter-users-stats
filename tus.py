3faab7bda3970bad4569e654fdadc85374bbc07f
Davide Gessa <gessadavide@gmail.com>
2016-03-19 16:04:19 +0100

    tweets

--- tus.py
+++ tus.py
@@ -9,9 +9,10 @@ conf = { "accounts": ["dagide"], "destination": "data/"}
 
 def getUser (account):
 	r = requests.get ('http://twitter.com/' + account)
-	followers = r.text.split ('ProfileNav-value')[1].split ('>')[1].split ('<')[0]
-	following = r.text.split ('ProfileNav-value')[2].split ('>')[1].split ('<')[0]
-	return (followers, following)
+	tweets = r.text.split ('ProfileNav-value')[1].split ('>')[1].split ('<')[0].replace (' K', '000').replace ('.', '').replace (',', '')
+	following = r.text.split ('ProfileNav-value')[2].split ('>')[1].split ('<')[0].replace (' K', '000').replace ('.', '').replace (',', '')
+	followers = r.text.split ('ProfileNav-value')[3].split ('>')[1].split ('<')[0].replace (' K', '000').replace ('.', '').replace (',', '')
+	return (tweets, followers, following)
 
 try:
     with open ('config.json', 'r') as f:
@@ -22,7 +23,6 @@ except:
         f.close ()
         print ('config.json created; restart the software after editing it')
         sys.exit (0)
-
 
 try:
 	os.mkdir (conf['destination'])
@@ -57,27 +57,47 @@ except:
 	fwing.close ()
 
 
+try:
+	ftw = open (conf['destination'] + 'tweets.csv', 'r')
+	d = ftw.readline ().replace ('\n', '')
+	conf['accounts'] = d.split (',')[1:]
+except:
+	ftw = open (conf['destination'] + 'tweets.csv', 'w')
+
+	ftw.write ('time')
+	for acc in conf['accounts']:
+		ftw.write (',' + acc)
+	ftw.write ('\n')
+	ftw.close ()
+
+
 if True:
 	fwers = open (conf['destination'] + 'followers.csv', 'a')
 	fwing = open (conf['destination'] + 'following.csv', 'a')
+	ftw = open (conf['destination'] + 'tweets.csv', 'a')
 
 	t = datetime.datetime.now().ctime ()
 	fwers.write (t)
 	fwing.write (t)
+	ftw.write (t)
 
 	for acc in conf['accounts']:
-		print ('Inspecting %s' % acc)
 		try:
 			p = getUser (acc)
-			fwers.write (',' + str (p[0]))
-			fwing.write (',' + str (p[1]))
+			print ('Inspecting %s' % acc, p)
+			fwers.write (',' + str (p[1]))
+			ftw.write (',' + str (p[0]))
+			fwing.write (',' + str (p[2]))
 		except:
+			ftw.write (',')
 			fwers.write (',')
 			fwing.write (',')
 
+	ftw.write ('\n')
 	fwers.write ('\n')
 	fwing.write ('\n')		
 
+	ftw.close ()
 	fwers.close ()
 	fwing.close ()	
 
